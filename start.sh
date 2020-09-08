5bc1ecb96927dec939814d9522d4d9165a8d125c
pakhiyakomaro <70978558+pakhiyakomaro@users.noreply.github.com>
2020-09-09 01:58:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 docker volume create kalivol
+echo "volume created"
 docker run -d --network host --privileged -v $HOME:/home/$USER --mount source=kalivol,target=/opt/vol/ newlinedotblog/kalidesktop
+echo "run complete"
